f661328636e102a1c48757e928f732bf4f4f7deb
residentvk-ship-it <residentvk@gmail.com>
2026-08-20 17:05:59 +0300

    coment

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  logger: true, // Включает базовые логи транзакций в консоли
-  debug: true,  // Включает детализированные SMTP-логи (трафик сервера)
+  //logger: true, // Включает базовые логи транзакций в консоли
+  //debug: true,  // Включает детализированные SMTP-логи (трафик сервера)
 })
 
 type OrderItem = {
